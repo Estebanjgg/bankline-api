88aceaaf435901e202587435de46749529bbc6d0
Esteban Jose Gonzalez Gomez <91167097+Estebanjgg@users.noreply.github.com>
2022-05-04 18:33:47 -0300

    importaciones

---
+++ src/main/java/com/dio/santander/banklineapi/controller/CorrentistaController.java
@@ -0,0 +1,19 @@
+package com.dio.santander.banklineapi.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dio.santander.banklineapi.model.Correntista;
+import com.dio.santander.banklineapi.repository.CorrentistaRepository;
+
+@RestController
+@RequestMapping("/correntistas")
+public class CorrentistaController {
+	
+	private 
+	public List<Correntista> findAll(){
+		
+	}
+}
